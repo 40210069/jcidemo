3195b5d25f2b2e26405b06fde693f84edbb0d495
40210069 <slivingstone07@qub.ac.uk>
2020-10-20 13:15:14 +0100

    Divide

--- src/uk/ac/qub/eeecs/dave/uk/ac/qub/eeecs/dave/CalculatorTest.java
+++ src/uk/ac/qub/eeecs/dave/uk/ac/qub/eeecs/dave/CalculatorTest.java
@@ -1,6 +1,7 @@
 package uk.ac.qub.eeecs.dave;
 
 import static org.junit.jupiter.api.Assertions.*;
+import uk.ac.qub.eeecs.dave.Calculator;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,13 +9,13 @@ class CalculatorTest {
 
 	@Test
 	void testAdd() {
-		Calculator c = new Calculator();
+		uk.ac.qub.eeecs.dave.Calculator c = new uk.ac.qub.eeecs.dave.Calculator();
 		assertEquals(c.add(10,1), 11);
 	}
 	
 	@Test
 	void testSub() {
-		Calculator c = new Calculator();
+		uk.ac.qub.eeecs.dave.Calculator c = new uk.ac.qub.eeecs.dave.Calculator();
 		assertEquals(c.sub(10,1), 9);
 	}
 }
